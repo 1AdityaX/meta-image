297251ed4f9966826afc61b576f03ada721bad68
Aditya <82662265+AnonymousXsn@users.noreply.github.com>
2022-03-09 09:37:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,16 +9,21 @@ from PIL.ExifTags import TAGS # Nessacary imports
 from PIL import Image
 import os
 
+
+def image_osint(image):
+    imagen = os.path.abspath(image) #Gets the absolute path of the image
+    image = Image.open(imagen) #Opens the image and gives a image object
+
+    exifdata = image.getexif() # Gets the exif data of the image (meta data)
+
+    for tag_id in exifdata: #loops through the exif data
+        tag = TAGS.get(tag_id, tag_id) # Gets the data tags
+        data = exifdata.get(tag_id)  # Gets the data inside it 
+        if isinstance(data, bytes):
+            data = data.decode() # decodes the data
+        return f"{tag:25}: {data}" #return the meta-data
+
 imagename = "image.jpg" #image path
-imagen = os.path.abspath(imagename) #Gets the absolute path of the image
-image = Image.open(imagen) #Opens the image and gives a image object
-
-exifdata = image.getexif() # Gets the exif data of the image (meta data)
-
-for tag_id in exifdata: #loops through the exif data
-    tag = TAGS.get(tag_id, tag_id) # Gets the data tags
-    data = exifdata.get(tag_id)  # Gets the data inside it 
-    if isinstance(data, bytes):
-        data = data.decode() # decodes the data
-    print(f"{tag:25}: {data}") #prints the meta-data
+metadata = image_osint(imagename)
+print(metadata)
     
